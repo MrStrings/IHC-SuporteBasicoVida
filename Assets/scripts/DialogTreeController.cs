fb840dc3121f852bd969818a9bb58da27da1069b
MrStrings <phstring95@hotmail.com>
2016-06-04 13:40:38 -0300

    initial commit

---
+++ Assets/scripts/DialogTreeController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogTreeController : MonoBehaviour {
+
+	public Text situation;
+	public Text[] choice;
+
+
+	// Use this for initialization
+	void Start () {
+		situation.text = "Me salva";
+		choice [0].text = "Agora";
+		choice [1].text = "Depois...";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/RhythmButtonHit.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RhythmButtonHit : MonoBehaviour {
+
+	public enum State {Increasing, Decreasing};
+
+	public float min, max;
+	public float speed;
+	public float min_correct, max_correct;
+	public float currentValue;
+
+
+	public State state = State.Decreasing;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		UpdateValue ();
+	}
+
+
+
+	void UpdateValue() {
+		if (state == State.Decreasing) {
+			if (currentValue < min) {
+				state = State.Increasing;
+			}
+
+			currentValue -= (Time.deltaTime * speed);
+
+		} else {
+			if (currentValue > max) {
+				state = State.Increasing;
+			}
+
+			currentValue += (Time.deltaTime * speed);
+		}
+
+	}
+}
