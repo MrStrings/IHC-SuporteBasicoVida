0f890da855e320444e6e09d535938ee3bcd92b38
MrStrings <phstring95@hotmail.com>
2016-06-26 11:33:32 -0300

    minigame scripts e controle de pontuacao

---
+++ Assets/scripts/PointsManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointsManager : MonoBehaviour {
+
+
+	public static float points;
+
+	public static float initialPoints;
+	public bool resetOnSceneLoad;
+
+
+	// Use this for initialization
+	void Start () {
+		if (resetOnSceneLoad)
+			points = initialPoints;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (points <= 0) {
+			//GAME OVER
+		}
+	}
+}
